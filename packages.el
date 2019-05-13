eb370d0c5fc043674d8b68ae816667c565184756
Anmol Jagetia <anmol.j@media.net>
2019-05-13 16:12:45 +0530

    Removes . and replaces it with dashes

--- packages.el
+++ packages.el
@@ -1,9 +1,9 @@
-(defconst my-ch.sh-layer-packages
+(defconst my-ch-sh-layer-packages
   '(
     (cheat-sh :location (recipe
                          :fetcher github
                          :repo "davep/cheat-sh.el"))
     ))
 
-(defun my-ch.sh-layer/init-cheat-sh ()
+(defun my-ch-sh-layer/init-cheat-sh ()
   (use-package cheat-sh))
