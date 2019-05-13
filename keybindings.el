afba9126c025d26a93657c9502a4425c37c586cc
Anmol Jagetia <anmol.j@media.net>
2019-05-13 16:06:05 +0530

    Adds the relevant keybindings

---
+++ keybindings.el
@@ -0,0 +1,9 @@
+;; Labels the app as Cheatsheet se it doesn't appear as "prefix" in the menu
+(spacemacs/declare-prefix "a C" "Cheatsheet")
+
+;; The remaining useful keybindings to using Leetcode
+(spacemacs/set-leader-keys
+ "a C l" 'cheat-sh
+ "a C r" 'cheat-sh-region
+ "a C s" 'cheat-sh-search
+ )
