67d07cadab93f53c094417c32371e036f94cef8c
Anmol Jagetia <anmol.j@media.net>
2019-05-20 18:06:13 +0530

    Updates to use the language-sensitive branch

--- packages.el
+++ packages.el
@@ -2,7 +2,8 @@
   '(
     (cheat-sh :location (recipe
                          :fetcher github
-                         :repo "davep/cheat-sh.el"))
+                         :repo "davep/cheat-sh.el"
+                         :branch "language-sensitive"))
     ))
 
 (defun my-ch-sh-layer/init-cheat-sh ()
